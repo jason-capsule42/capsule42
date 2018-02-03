5b3329e8fcd35c57225e5c4b22152f0d346995ba
jason-capsule42 <jason@capsule42.com>
2018-02-02 16:35:16 -0800

    Cleanup Resume UI

--- src/app/components/resume-developer/resume-developer.component.ts
+++ src/app/components/resume-developer/resume-developer.component.ts
@@ -7,6 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResumeDeveloperComponent implements OnInit {
 
+  jobs = [
+    {
+      dates: '2016 - Present',
+      title: 'Business Owner, Web Developer',
+      company: 'Capsule 42, LLC',
+      info: 'Freelance web design & development'
+    },
+    {
+      dates: '2015 - 2016',
+      title: 'Dev Lead, Sr. Web Developer',
+      company: 'Vizualiiz, Inc.',
+      info: 'Web design & development, iOS & Android app development'
+    },
+    {
+      dates: '2014 - 2015',
+      title: 'Web Developer, Content Manager',
+      company: 'Verathon, Inc.',
+      info: 'CMS architecture & development, content management, feature design & development'
+    },
+    {
+      dates: '2013 - 2014',
+      title: 'Web Developer, Email Developer',
+      company: 'Classmates.com, Memory Lane',
+      info: 'Weekly content development HTML Email development'
+    },
+    {
+      dates: '2011 - 2013',
+      title: 'Release Manager, Content Developer',
+      company: 'Microsoft – MSN.com',
+      info: 'Weekly content release management HTML content development and conversion'
+    },
+    {
+      dates: '2007 - 2010',
+      title: 'Test Engineer, Web Developer',
+      company: 'Microsoft Hardware – Mouse and Keyboard',
+      info: 'Software test planning, execution and reporting UI development on product web site'
+    },
+    {
+      dates: '2001 - 2007',
+      title: 'Test Engineer',
+      company: 'Microsoft Games Studio',
+      info: 'Software test planning, execution and reporting'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
--- src/app/components/social-links/social-links.component.ts
+++ src/app/components/social-links/social-links.component.ts
@@ -10,6 +10,33 @@ export class SocialLinksComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    (<any>$(window)).scroll(function() {
+      const startPos = 320;
+      const stopPos = 365;
+
+      if ((<any>$(window)).scrollTop() >= startPos) {
+        (<any>$('app-social-links')).addClass('lock');
+        (<any>$('.grid')).css({ 'line-height': 45 + 'px' });
+        (<any>$('.grid a')).css({ 'font-size': 15 + 'px' });
+
+        if ((<any>$(window)).scrollTop() <= stopPos) {
+          const calc = 45 + stopPos - (<any>$(window)).scrollTop();
+          console.log('calc:', calc);
+
+          (<any>$('.grid')).css({ 'line-height': calc + 'px' });
+          (<any>$('.grid a')).css({ 'font-size': calc / 3 + 'px' });
+
+          (<any>$('.social-spacer')).css({ 'height': 75 + 0 + 'px' });
+        }
+
+      } else {
+        (<any>$('app-social-links')).removeClass('lock');
+        (<any>$('.grid')).css({ 'line-height': 90 + 'px' });
+        (<any>$('.grid a')).css({ 'font-size': 30 + 'px' });
+
+        (<any>$('.social-spacer')).css({ 'height': 0 + 'px' });
+      }
+    });
   }
 
 }
