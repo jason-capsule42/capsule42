913818d734d37d5815c70a0c280d24b8a2a87bd5
jason-capsule42 <jason@capsule42.com>
2018-02-08 06:08:39 -0800

    Temporarily handle broken ngFor loops in template under IE

--- src/app/components/dev-portfolio/dev-portfolio.component.ts
+++ src/app/components/dev-portfolio/dev-portfolio.component.ts
@@ -7,9 +7,66 @@ import { Component, ViewChildren, OnInit, AfterViewInit, HostListener } from '@a
 })
 export class DevPortfolioComponent implements OnInit, AfterViewInit {
 
-  sitesLoaded = [];
+  public test;
 
-  sites = [
+  people = [
+    { id: 1, name: { forename: 'John', surname: 'Doe' } },
+    { id: 2, name: { forename: 'John', surname: 'Smith' } },
+    { id: 3, name: { forename: 'Peter', surname: 'Scott' } },
+    { id: 4, name: { forename: 'Sue', surname: 'Reece' } }
+  ];
+
+  sites: Object[] = [];
+
+  sitesTest = [
+    {
+      id: 1,
+      data: {
+        type: 'web',
+        label: 'Whipir',
+        description: '',
+        tab: 'Whispir',
+        image: 'assets/images/portfolio/whispir.gif',
+        image_alt: 'whispir.com',
+        url: 'http://www.whispir.com'
+      }
+    },
+    {
+      id: 2,
+      data: {
+        type: 'web',
+        label: 'Verathon',
+        description: '',
+        tab: 'Verathon',
+        image: 'assets/images/portfolio/verathon.gif',
+        image_alt: 'verathon.com'
+      }
+    },
+    {
+      id: 3,
+      data: {
+        type: 'web',
+        label: 'Get PiMP',
+        description: '',
+        tab: 'Get PiMP',
+        image: 'assets/images/portfolio/pimp.gif',
+        image_alt: 'getpimp.org'
+      }
+    },
+    {
+      id: 4,
+      data: {
+        type: 'mobile-app',
+        label: 'Vizualiiz',
+        description: '',
+        tab: 'Scout | Vizualiiz',
+        image: 'assets/images/portfolio/scout.gif',
+        image_alt: 'Scout App'
+      }
+    }
+  ];
+
+  sitesList = [
     {
       type: 'web',
       label: 'Whipir',
@@ -32,7 +89,7 @@ export class DevPortfolioComponent implements OnInit, AfterViewInit {
       label: 'Get PiMP',
       description: '',
       tab: 'Get PiMP',
-      image: 'assets/images/portfolio/verathon.gif',
+      image: 'assets/images/portfolio/pimp.gif',
       image_alt: 'getpimp.org'
     },
     {
@@ -45,6 +102,44 @@ export class DevPortfolioComponent implements OnInit, AfterViewInit {
     }
   ];
 
+  gigs = [
+    {
+      image: '',
+      icon: 'msn',
+      iconType: 'monoSocialIcons',
+      imgUrl: '',
+      label: 'msn | autos'
+    },
+    {
+      image: '',
+      icon: '',
+      iconType: 'socicon-microsoft',
+      imgUrl: '',
+      label: 'Microsoft | Hardware'
+    },
+    {
+      image: '',
+      icon: '',
+      iconType: '',
+      imgUrl: '',
+      label: 'Classmates.com'
+    },
+    {
+      image: '',
+      icon: '',
+      iconType: '',
+      imgUrl: 'assets/images/portfolio/cbe-logo.png',
+      label: ''
+    },
+    {
+      image: '',
+      icon: '',
+      iconType: '',
+      imgUrl: 'assets/images/portfolio/cascadia.png',
+      label: ''
+    }
+  ];
+
   @ViewChildren('portfolioSites') pSites: any;
 
   @HostListener('window:resize') resize() {
@@ -52,7 +147,19 @@ export class DevPortfolioComponent implements OnInit, AfterViewInit {
     this.resizeAppFrames();
   }
 
-  constructor() { }
+  constructor() {
+    this.test = Object.keys(this.sitesList);
+    console.log(this.test);
+  }
+
+  trackBySite(index: number, site: any) {
+    return site.id;
+  }
+
+  trackByPeople(index: number, person: any) {
+    return person.id;
+  }
+
   resizeAppFrames() {
     let smallest = 0;
 
@@ -106,6 +213,10 @@ export class DevPortfolioComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit() {
+    for (let i = 0; i < this.sitesList.length; i++) {
+      // console.log(this.sitesList[i]);
+      this.sites.push(this.sitesList[i]);
+    }
   }
 
 }
